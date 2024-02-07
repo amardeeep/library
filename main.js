59581e90dd1c1348d191adbfcd8c61d87639bd0f
Amardeep <amardeepdeepak2016@gmail.com>
2024-02-07 23:23:22 +0530

    create object constructor
    to create book objects

--- main.js
+++ main.js
@@ -7,3 +7,18 @@ function Book(){
 function addBookToLibrary(bookObject){
     myLibrary.push(bookObject);
 }
+//objectConstructor
+function Books(title,author,pages,read){
+    this.title=title;
+    this.author=author;
+    this.pages=pages;
+    this.read=read;
+    this.info=function(){
+        if(this.read==true){
+            return this.title+" by "+this.author+", read."
+        }
+        else{
+            return this.title+" by "+this.author+", not read yet."
+        }
+    }
+}
