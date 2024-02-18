f3b35d932421b8118d7f4da5ba3cf49e6d64774c
Amardeep <amardeepdeepak2016@gmail.com>
2024-02-18 14:30:37 +0530

    Finish writting logic for library

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const myLibrary=[];
 
 function addBookToLibrary(bookObject){
     myLibrary.push(bookObject);
+    return myLibrary;
 }
 //objectConstructor
 function Books(title,author,pages,read){
@@ -25,27 +26,76 @@ function Books(title,author,pages,read){
 const book2= new Books("title2","author2","pages2",false);
 addBookToLibrary(book1);
 addBookToLibrary(book2);*/
-
+//function to clear all divs
+function clearDivs(){
+    const Divs=document.querySelectorAll('.newDiv');
+    Divs.forEach(div=>{
+        div.remove();
+    })
+}
+/*
+function removeBtn(){
+    const divs=document.querySelectorAll('.newDiv');
+    divs.forEach(div=>{
+        const removeBtn=document.createElement('button');
+        removeBtn.innerHTML="Remove Button";
+        div.appendChild(removeBtn);
+        removeBtn.addEventListener("click",()=>{
+            const indexToBeRemoved=div.dataset.number;
+            myLibrary.splice(indexToBeRemoved,1);
+            createBooksDiv(myLibrary);
+    
+        })
+    });
+    }*/
 //create a loop to iterate through array and display it on a div.
 const cardsDiv=document.querySelector(".cards");
-function displayBooks(myLibrary){
+function createBooksDiv(myLibrary){
+    clearDivs();
     for(let i=0;i<myLibrary.length;i++){
         const newDiv= document.createElement('div');
         cardsDiv.appendChild(newDiv);
-        const divTitle= document.createElement('h1');
-        const divAuthor= document.createElement('h2');
-        const divPages= document.createElement('h2');
-        const divRead= document.createElement('button');
+        const divTitle= document.createElement('h3');
+        const divAuthor= document.createElement('h3');
+        const divPages= document.createElement('h3');
+        const divRead= document.createElement('h3');
+        const divHeader=document.createElement('h1');
+        divHeader.innerHTML="Book"+i;
         divTitle.innerHTML=myLibrary[i].title;
         divAuthor.innerHTML=myLibrary[i].author;
         divPages.innerHTML=myLibrary[i].pages;
         divRead.innerHTML=myLibrary[i].read;
+        newDiv.appendChild(divHeader);
         newDiv.appendChild(divTitle);
         newDiv.appendChild(divAuthor);
         newDiv.appendChild(divPages);
         newDiv.appendChild(divRead);
+        /*divAuthor.dataset.author=myLibrary[i].author;
+        divTitle.dataset.title=myLibrary[i].title;
+        divPages.dataset.pages=myLibrary[i].pages;
+        divRead.dataset.read=myLibrary[i].pages;*/
+        newDiv.setAttribute("class","newDiv");
+        newDiv.setAttribute("data-number",i)
+        const removeBtn=document.createElement("button");
+        newDiv.appendChild(removeBtn);
+        removeBtn.innerHTML="Remove Button";
+        removeBtn.addEventListener("click",()=>{
+            const indexToBeRemoved=i;
+            myLibrary.splice(indexToBeRemoved,1);
+            createBooksDiv(myLibrary);
+
+        })
+        const status=document.createElement('button');
+        status.innerHTML="Book Status";
+        status.addEventListener("click",()=>{
+            myLibrary[i].read=!myLibrary[i].read;
+            createBooksDiv(myLibrary);
+        })
+        newDiv.appendChild(status);
+        
     }
 }
+
 //create NEW BOOK button that displays a modal dialog form that takes input to generate a book object
 const dialog= document.querySelector("dialog");
 const newBtn= document.querySelector(".newBook");
@@ -64,8 +114,10 @@ form.addEventListener("submit",(event)=>{
     const pages=form.pages.value;
     const read=form.read.value;
     const newbook=new Books(title,author,pages,read); 
-    addBookToLibrary(newbook);
-    displayBooks(myLibrary);
+    createBooksDiv(addBookToLibrary(newbook));
 });
+Books.prototype.readStatus=function(){
+
     
+}
 
